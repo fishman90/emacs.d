dda6c0d8a7adda2e2b3500993412724779af2a1c
yuchunzhou <enjoylife.czzz@gmail.com>
2025-12-17 11:36:45 +0800

    config magit file icon

--- configs/vcs.el
+++ configs/vcs.el
@@ -3,5 +3,6 @@
 (use-package transient :commands transient--set-layout)
 
 (use-package magit
+  :custom (magit-format-file-function 'magit-format-file-nerd-icons)
   :hook (magit-status-mode . display-line-numbers-mode)
   :config (fishman-save-some-buffers-before 'magit-status))
