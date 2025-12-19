512f5480f2d5ef7d3f1d9098e4ec5c9e82bb4dff
yuchunzhou <enjoylife.czzz@gmail.com>
2025-12-19 15:31:39 +0800

    update

--- configs/verb.el
+++ configs/verb.el
@@ -5,3 +5,6 @@
   :config ; nofmt
   (fishman-save-current-buffer-before 'verb-send-request-on-point-display)
   (general-define-key :keymaps 'org-mode-map "C-c C-r" 'verb-send-request-on-point-display))
+
+(use-package curl-to-elisp
+  :config (fishman-save-current-buffer-before 'curl-to-elisp-verb))
