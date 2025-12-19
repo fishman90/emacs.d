70ccef08a66cafac782cea25567960f0592d0e2f
yuchunzhou <enjoylife.czzz@gmail.com>
2025-12-19 16:48:31 +0800

    update

--- configs/vcs.el
+++ configs/vcs.el
@@ -4,9 +4,14 @@
 
 (use-package magit
   :custom (magit-format-file-function 'magit-format-file-nerd-icons)
-  :bind ("C-x z" . magit-log-buffer-file)
-  :hook (magit-status-mode . display-line-numbers-mode)
+  :bind ; nofmt
+  ("C-x j" . magit-log-current)
+  ("C-x z" . magit-log-buffer-file)
+  :hook ; nofmt
+  (magit-status-mode . display-line-numbers-mode)
+  (magit-log-mode . display-line-numbers-mode)
   :config ((fishman-save-some-buffers-before 'magit-status)
+	   (fishman-save-some-buffers-before 'magit-log-current)
 	   (fishman-save-some-buffers-before 'magit-log-buffer-file)))
 
 (use-package magit-todos :after magit :config (magit-todos-mode))
