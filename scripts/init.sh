e59f3ceff85a55a283a7be7a245c565602b7e456
yuchunzhou <enjoylife.czzz@gmail.com>
2025-12-24 14:01:15 +0800

    remove unused config and scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,16 +2,11 @@
 
 alias emacs="emacs -nw -l ~/.emacs.d/init.el"
 
-# Elixir
-eval "$(mise activate zsh)" 2>/dev/null || true
 
 # Golang
 [ -f "$HOME/.g/env" ] && . "$HOME/.g/env"
 
 export GO111MODULE=on
-
-# Nim
-export PATH=~/.nimble/bin:$PATH
 
 # Python
 export PYENV_ROOT=~/.pyenv
@@ -22,8 +17,3 @@ eval "$(pyenv virtualenv-init -)" 2>/dev/null || true
 
 # Rust
 [ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
-
-# Zig
-export ZVM_INSTALL=~/.zvm/self
-export PATH=$ZVM_INSTALL:$PATH
-export PATH=~/.zvm/bin:$PATH
