0453cba13cf09ccb028dfcc6fd9d1ccf89a45335
yuchunzhou <enjoylife.czzz@gmail.com>
2026-01-03 11:09:19 +0800

    update

--- tool.sh
+++ tool.sh
@@ -3,9 +3,9 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y xclip xsel
 sudo apt install -y git
 sudo apt install -y wget
+sudo apt install -y xclip xsel
 
 sudo apt autoremove -y
 sudo apt autoclean
@@ -14,5 +14,5 @@ go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
 
 pip install --user --upgrade tombi
 
-cargo install --git https://github.com/BurntSushi/ripgrep.git --branch master ripgrep
 cargo install --git https://github.com/sharkdp/fd.git --branch master fd-find
+cargo install --git https://github.com/BurntSushi/ripgrep.git --branch master ripgrep
