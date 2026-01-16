58e09c38eef052df80d180847529081d00a6d07b
yuchunzhou <enjoylife.czzz@gmail.com>
2026-01-16 17:05:24 +0800

    use terminator

--- basic-tool.sh
+++ basic-tool.sh
@@ -3,9 +3,10 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y git
-sudo apt install -y wget
+sudo apt install -y terminator
 sudo apt install -y xclip xsel
+sudo apt install -y wget
+sudo apt install -y git
 
 sudo apt autoremove -y
 sudo apt autoclean
